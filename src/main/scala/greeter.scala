3ab2ae2795527b05c92a6498af85309ef6996b66
inca <incarnate@whiteants.net>
2013-03-19 09:48:11 +0400

    Added greeters for OOP lesson.

---
+++ src/main/scala/greeter.scala
@@ -0,0 +1,40 @@
+package sandbox
+
+class Greeter(val name: String) {
+
+  var prefix = "Hello, "
+  var suffix = "!"
+
+  def greeting = prefix + name + suffix
+
+  def greet() {
+    println(greeting)
+  }
+
+}
+
+class ParameterizedGreeter(val name: String,
+                           var prefix: String,
+                           var suffix: String) {
+
+  def greeting = prefix + name + suffix
+
+  def greet() {
+    println(greeting)
+  }
+
+}
+
+class MutableGreeter {
+
+  var name: String = "World"
+  var prefix: String = "Hello, "
+  var suffix: String = "!"
+
+  def greeting = prefix + name + suffix
+
+  def greet() {
+    println(greeting)
+  }
+
+}
