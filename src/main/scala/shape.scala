b460727b5d697ec7e64b52d5a6d8e46e00905d40
inca <incarnate@whiteants.net>
2013-04-01 20:57:44 +0400

    Added some extensions along the lesson.

--- src/main/scala/shape.scala
+++ src/main/scala/shape.scala
@@ -1,22 +1,49 @@
 package sandbox
 
-abstract class Shape {
+trait Shape extends Comparable[Shape] {
 
   def perimeter: Double
 
   def area: Double
 
+  def compareTo(o: Shape) = this.area.compareTo(o.area)
 }
 
-class Circle(val radius: Double) extends Shape {
+trait Radial {
 
-  def diameter = 2 * radius
+  def radius: Double
 
-  def perimeter = math.Pi * diameter
+  def angle: Double
+
+  final def diameter = 2 * radius
+
+  final def arc = radius * angle
+
+}
+
+final class Circle(val radius: Double)
+    extends Shape
+    with Radial {
+
+  def angle = 2 * math.Pi
+
+  def perimeter = arc
 
   def area = math.Pi * radius * radius
 
 }
+
+final class Sector(val radius: Double,
+                   val angle: Double)
+    extends Shape
+    with Radial {
+
+  def perimeter = diameter + arc
+
+  def area = arc * radius / 2
+
+}
+
 
 class Triangle(val ab: Double,
                val ac: Double,
@@ -39,16 +66,26 @@ class Rectangle(val ab: Double,
                 val ac: Double)
     extends Shape {
 
-  def cd = ab
+  final def cd = ab
 
-  def bd = ac
+  final def bd = ac
 
   def bc = math.sqrt(ab * ab + ac * ac)
 
-  def ad = bc
+  final def ad = bc
 
   def perimeter = 2 * (ab + ac)
 
   def area = ab * ac
 
 }
+
+final class Square(val side: Double)
+    extends Rectangle(side, side) {
+
+  override def perimeter = 4 * side
+
+  override def bc = math.sqrt(2) * side
+
+}
+
